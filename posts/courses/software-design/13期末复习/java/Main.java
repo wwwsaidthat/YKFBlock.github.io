bc2fcd2497893b1327b683911de27df7e157e657
wwwsaidthat <978887223@qq.com>
2026-05-06 14:56:39 +0800

    update

--- posts/courses/software-design/13期末复习/java/Main.java
+++ posts/courses/software-design/13期末复习/java/Main.java
@@ -1,23 +1,32 @@
 public class Main{
   public static void main(String[] args){
-    Facade fc = new Facade();
-    System.out.println(fc.prove());
+    Star star2 = new RealStar();
+    star2.act();
+    Star star = new ProxyAgent(new RealStar());
+    star.act();
+    
   }
 }
-class SubFlow1{
-  public boolean isTrue(){return true;}
+
+abstract class Star{
+  public abstract void act();
 }
-class SubFlow2{
-  public boolean isOk(){return true;}
+class RealStar extends Star{
+  @Override
+  public void act() {
+    System.out.println("明星认真拍戏");
+  }
 }
-class SubFlow3{
-  public boolean isSafe(){return true;}
-}
-class Facade{
-  SubFlow1 sf1 = new SubFlow1();
-  SubFlow2 sf2 = new SubFlow2();
-  SubFlow3 sf3 = new SubFlow3();
-  public boolean prove(){
-    return sf1.isTrue()&&sf2.isOk()&&sf3.isSafe();
+class ProxyAgent extends Star{
+  private RealStar realStar;
+  public ProxyAgent(RealStar realStar){
+    this.realStar = realStar;
+  }
+  @Override
+  public void act() {
+    System.out.println("谈合同");
+    System.out.println("接广告");
+    realStar.act();
+    System.out.println("收工");
   }
 }
